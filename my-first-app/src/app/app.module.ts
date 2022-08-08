5a0ef6e088ec453d8018147d5837c1de1ecde146
ashaik16 <ashaik16@depaul.edu>
2022-08-08 15:48:53 -0500

    Created the server component manually

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ServerComponent } from './server/server.component';
 import { AppComponent } from './app.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
