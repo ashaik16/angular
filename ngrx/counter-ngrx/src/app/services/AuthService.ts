094ad5bf04bfbaffd16454de7df2619dd0c73f26
ashaik16 <ashaik16@depaul.edu>
2022-08-21 18:13:40 -0500

    ngrx changed the login url that was a signup url

--- ngrx/counter-ngrx/src/app/services/AuthService.ts
+++ ngrx/counter-ngrx/src/app/services/AuthService.ts
@@ -10,10 +10,10 @@ import { User } from '../models/user.model';
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
-
+  //Note: signup with the below url for the first time then change it to the down url once yoy have registered via login to email and password just use the below becuase now the data is saved in db now you can perform login once it is sgned uphttps://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=
   login(email: String, password: String) {
     return this.http.post<AuthResponseData>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.FIREBASE_API_KEY}`,
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.FIREBASE_API_KEY}`,
       { email, password, returnSecureToken: true }
     );
   }
